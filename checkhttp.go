46c261bd37cda72362a709172091fd6b0e6c61a7
GMBuilding <136167080+GMBuilding@users.noreply.github.com>
2024-05-02 14:41:08 +0000

    a

---
+++ checkhttp.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+	"os"
+	"sync"
+)
+
+const (
+	concurrency = 100
+)
+
+func main() {
+	// 读取 IP 列表
+	ips, err := readLines("ips.txt")
+	if err != nil {
+		fmt.Println("Failed to read IP list:", err)
+		return
+	}
+
+	// 创建等待组
+	var wg sync.WaitGroup
+	wg.Add(len(ips))
+
+	// 限制并发请求的数量
+	semaphore := make(chan struct{}, concurrency)
+
+	// 遍历 IP 列表
+	for _, ip := range ips {
+		semaphore <- struct{}{} // 占用一个信号量
+		go func(ip string) {
+			defer wg.Done()
+			defer func() { <-semaphore }() // 释放一个信号量
+
+			// 请求 IP:54321
+			resp, err := http.Get("http://" + ip + ":54321")
+			if err != nil {
+				fmt.Printf("Error fetching %s: %s\n", ip, err)
+				return
+			}
+			defer resp.Body.Close()
+
+			// 判断 HTTP 状态码是否为 200
+			if resp.StatusCode == http.StatusOK {
+				fmt.Printf("%s returned status code 200\n", ip)
+				writeLine("ips_new.txt", ip)
+			}
+		}(ip)
+	}
+
+	// 等待所有请求完成
+	wg.Wait()
+	fmt.Println("All requests completed.")
+}
+
+// readLines 从文件中读取每行内容并返回字符串切片
+func readLines(filename string) ([]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+// writeLine 将字符串写入文件的新一行
+func writeLine(filename, line string) error {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	if _, err := file.WriteString(line + "\n"); err != nil {
+		return err
+	}
+	return nil
+}
---
+++ last.py
@@ -0,0 +1,30 @@
+#  -*- coding: UTF-8 -*-
+
+import requests
+timeout = (3, 3)
+
+def check(ip):
+	try: 
+		response = requests.post((str((str((str("http://"))) + str(ip))) + str((str(":54321/login")))), data={'username': 'admin', 'password': 'admin'}, timeout=timeout)
+	except:
+		print(ip + "Failed")
+		return False
+	try: 
+		if response.json().get("success") == 'True' or response.json().get("success") == 'true': 
+			print(ip + "OK")
+			return True
+		else:
+			print(ip + "Failed")
+			return False
+	except:
+		return False
+	
+with open("ips_new.txt", "r") as file:
+	for line in file:
+		# 提取IP地址
+		ip = line.strip()
+		b = check(ip)
+		if b == True:
+			fileObj = open("res.txt", "a", encoding="UTF8")
+			fileObj.write(ip + "\n")
+			fileObj.close()
