e2e11f6be112c93f2c88ddd4b1be830cdb934772
GMBuilding <136167080+GMBuilding@users.noreply.github.com>
2024-08-04 06:01:12 +0000

    asd

--- mass.sh
+++ mass.sh
@@ -1,3 +1,3 @@
 masscan -p54321 --max-rate 8000 -oG results.txt --exclude 255.255.255.255 \
 
-grep -E -o '([0-9]{1,3}\.){3}[0-9]{1,3}' results.txt > ips.txt
+grep -E -o '([0-9]{1,3}\.){3}[0-9]{1,3}' results.txt >> ips.txt
