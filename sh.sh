74daf8c2b9d31c20be640465397d76895fb24858
GMBuilding <136167080+GMBuilding@users.noreply.github.com>
2024-08-04 05:48:02 +0000

    Sun Aug  4 05:48:02 UTC 2024

--- sh.sh
+++ sh.sh
@@ -1,1 +1,12 @@
-ASN="54574"; curl -s https://ipinfo.io/AS${ASN} -H "User-Agent: User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0" | grep -oE '\b[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/[0-9]+\b' >> ipranges.txt
+#!/bin/bash
+
+ASNs=("123")
+
+# 遍历 ASN 数组
+for ASN in "${ASNs[@]}"
+do
+  # 使用 curl 命令获取 IP 范围
+  curl -s https://ipinfo.io/AS${ASN} -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0" |   grep -oE '\b[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/[0-9]+\b' >> ipranges.txt
+done
+
+curl https://download.microsoft.com/download/7/1/D/71D86715-5596-4529-9B13-DA13A5DE5B63/ServiceTags_Public_20240729.json |   grep -oE '\b[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/[0-9]+\b' > ipranges.txt
