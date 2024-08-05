726eb14043278f26feff62b41e1153fa9cb9c7f7
GMBuilding <136167080+GMBuilding@users.noreply.github.com>
2024-08-05 10:54:47 +0000

    ssa

--- sh.sh
+++ sh.sh
@@ -9,4 +9,4 @@ do
   curl -s https://ipinfo.io/AS${ASN} -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0" |   grep -oE '\b[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/[0-9]+\b' >> ipranges.txt
 done
 
-curl https://raw.githubusercontent.com/femueller/cloud-ip-ranges/fa157ef53be42c0d9f916351ae6e93ddfef80276/vultr-all-ip-ranges-geofeed.constant.com.json  |   grep -oE '\b[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/[0-9]+\b' >> ipranges.txt
+curl https://raw.githubusercontent.com/femueller/cloud-ip-ranges/master/aws-ip-ranges.json  |   grep -oE '\b[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/[0-9]+\b' >> ipranges.txt
