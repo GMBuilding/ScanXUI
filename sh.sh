48e0f62bb5c34182d66283f1ea5e35e1151aee8a
GMBuilding <136167080+GMBuilding@users.noreply.github.com>
2024-08-08 10:02:13 +0000

    Done

--- sh.sh
+++ sh.sh
@@ -9,4 +9,5 @@ do
   curl -s https://ipinfo.io/AS${ASN} -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0" |   grep -oE '\b[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/[0-9]+\b' >> ipranges.txt
 done
 
-curl https://raw.githubusercontent.com/femueller/cloud-ip-ranges/master/aws-ip-ranges.json  |   grep -oE '\b[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/[0-9]+\b' >> ipranges.txt
+#curl https://raw.githubusercontent.com/femueller/cloud-ip-ranges/master/aws-ip-ranges.json  |   grep -oE '\b[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/[0-9]+\b' >> ipranges.txt
+grep -E -o '([0-9]{1,3}\.){3}[0-9]{1,3}' nb.txt > ips.txt
